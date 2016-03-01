3e221690e8d95cbb1529379c0d1418b193843bb2
Helena Helgadóttir <helenah90@gmail.com>
2016-03-01 10:51:03 +0100

    navigation

---
+++ nav.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 nav-bg">
+				    <img alt="logo" src="img/christmaslogoW.png" class="logo">
+                    <a href="" class="right">Log in</a>
+                    <a href"" class="right">Courses</a>
+                    <a href="" class="right">About us</a>
+				
+			</div>
+			
+		</div>
+</body>
+</html>
