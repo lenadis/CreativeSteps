6c90ac4d3afbccdb3c310f2b92423c1354cbc2e4
Helena Helgadóttir <helenah90@gmail.com>
2016-03-01 11:04:00 +0100

    added link to stylesheet

--- homepage.php
+++ homepage.php
@@ -3,10 +3,19 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <body>
     <div class="container-fluid">
