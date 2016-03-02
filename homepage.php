be653c1188274a634cbb71eea1a0d6834d8bda03
Helena Helgadóttir <helenah90@gmail.com>
2016-03-02 22:15:22 +0100

    Small changes and fixes

--- homepage.php
+++ homepage.php
@@ -2,22 +2,6 @@
     include 'nav.php';
 ?>
 
-<!DOCTYPE html>
-<html class="no-js" lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
-    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="css/main.css">
-    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-</head>
-<body>
 <!--     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
---
+++ welcomeback.php
@@ -0,0 +1,23 @@
+<?php 
+    include 'nav.php'    
+?>
+	<div>
+		<a href="" "right"> <!-- the image for user  --></a>
+	</div>
+    <div class="background bg-blue page"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <!-- add name of user -->
+                <p class="big-center2"> Welcome back X </p> 
+            </div>
+        </div> 
+	    <img class="icon" src="img/camerawhite.png"> 
+	    <img class="bar-width" src="img/bar.png"></img>    
+	    <button type="button" class="btn btn-primary img-center">Continue</button>
+	</div>
+
+
+    </div>
+</body>
+</html>
