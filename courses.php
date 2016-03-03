5a240ca19e5c1e17a8d94cb3e4c3d957ad740963
Helena Helgadóttir <helenah90@gmail.com>
2016-03-03 10:27:10 +0100

    small changes

--- courses.php
+++ courses.php
@@ -5,25 +5,29 @@
     <div class="container">
 	    <div class="row">
 	    	<p class="big-center2"> Subjects </p>
-	    	<div class="row">
+	    	<div class="row padding-top-bottom">
 	    		<div class="col-md-3">
-	    			<a href=""><img class="icon"src="img/paint.png"></a>
+	    			<a href=""><img class="icon"src="img/artwhite.png"></a>
+	    			<a href=""><p class="subtitle text-center padding-top-bottom"> Art </p></a>
 	    		</div>
 	    		<div class="col-md-3">
-	    			<a href=""><img class="icon" src="img/pencil.png"></a>
+	    			<a href=""><img class="icon" src="img/drawingwhite.png"></a>
+	    			<a href=""><p class="subtitle text-center padding-top-bottom"> Creative writing</p></a>
 	    		</div>
 	    		<div class="col-md-3">
-	    			<a href=""><img class="icon"src="img/sewing.png"></a>
+	    			<a href=""><img class="icon"src="img/sewingwhite.png"></a>
+	    			<a href=""><p class="subtitle text-center padding-top-bottom"> Design </p></a>
 	    		</div>
 	    		<div class="col-md-3">
-	    			<a href=""><img class="icon"src="img/guitar.png"></a>
+	    			<a href=""><img class="icon"src="img/guitarwhite.png"></a>
+	    			<a href=""><p class="subtitle text-center padding-top-bottom"> Music </p></a>
 	    		</div>
 	    	</div>
 	    </div>
-	    <p class="big-center2"> Popular courses </p>
+	    <p class="big-center"> Popular courses </p>
 	    <div class="row">
 	        <div class="col-md-4">
-	        <!-- 					Photo name of classes and maybe description? or show more button loop?-->	        	
+	        <!-- Photo name of classes and maybe description? or show more button loop? -->	        	
 	        </div>
 	    </div>      
 	</div>
