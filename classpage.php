9a0b12d2c2740acdc9502e0ba00006bd1f7aa51f
Helena Helgadóttir <helenah90@gmail.com>
2016-03-03 15:58:31 +0100

    class page with a video

---
+++ classpage.php
@@ -0,0 +1,22 @@
+<?php
+	include 'nav.php';
+?>
+<body>
+    <div class="background bg-blue page"></div>
+	<div class="container">
+		<div class="col-md-12">
+			<p class="padding-top big-center"> Introduction <!-- classname from db --> </p>
+		</div>
+		<div class="video-center">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/NpUzGNe42Cg" frameborder="0" allowfullscreen></iframe>
+		</div> 
+		<div>
+			<div class="row">
+			<p class="subtitle right">Next class</p>
+			</div>
+			<div="row">
+				<p class="right"> Our project <!-- next class so class_id + 1 or sth from db --> </p>
+			</div>
+		</div>
+</body>
+</html>
