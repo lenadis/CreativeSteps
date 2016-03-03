20730a3ff1298f6b798faefebce686c9fa7b31db
lenadis <lenadisr@gmail.com>
2016-03-03 23:04:40 +0100

    course photo upload

--- insertcourse.php
+++ insertcourse.php
@@ -1,21 +1,22 @@
 <?php
 session_start();
 include 'db.php';
-print_r($_SESSION);
+
 
 // Escape user inputs for security
 $name = $_POST['name'];
 $date = $_POST['date'];
-$photo = $_POST['photo'];
+//$photo = $_POST['photo'];
 $subject = $_POST['subject'];
 $price = $_POST['price'];
 $description = $_POST['description'];
 
 
-$stmt = $db->prepare("INSERT INTO course (creator_id, name, date, subject, price, description, photo) 
-                     VALUES (:creator_id,:name, :date, :subject, :price, :description, :photo)" );
+$stmt = $db->prepare("INSERT INTO course (creator_id, course_id, name, date, subject, price, description, photo) 
+                     VALUES (:creator_id, :course_id, :name, :date, :subject, :price, :description, :photo)" );
 
-$stmt->bindValue(':creator_id', $_SESSION['creator_id']);
+$stmt->bindValue(':creator_id', $_SESSION['user_id']);
+$stmt->bindValue(':course_id', $_SESSION['course_id']);
 $stmt->bindValue(':name', $name);
 $stmt->bindValue(':date', $date);
 $stmt->bindValue(':subject', $subject);
@@ -23,24 +24,40 @@ $stmt->bindValue(':price', $price);
 $stmt->bindValue(':description', $description);
 $stmt->bindValue(':photo', $_FILES['photo']['name']);
 
-$uploaddir = "img/";
-$uploadfile = $uploaddir . basename($_FILES['photo']['name']);
+$uploaddir = "img/course/";
         
 echo '<pre>';
-        
-if(move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)){
-    echo "File valid, photo successfully uploaded";
-    }
-else {
-    echo "File not valid, please try another photo";
-    }
 
 if ($stmt->execute()){
-    header("Location: homepage.php");
+    
+    $uploadfile = $uploaddir . basename($db->lastInsertId() . ".jpg");
+
+    if(move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)){
+    
+    }
+    else {
+        echo "File not valid, please try another photo";
+    }
+    header("Location: coursepage.php");
 }
 else {
     echo("ERRORERROEREROERO");
 }
-    
 
+        
+//if(move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)){
+//    echo "File valid, photo successfully uploaded";
+//    }
+//else {
+//    echo "File not valid, please try another photo";
+//    }
+//
+//if ($stmt->execute()){
+//    header("Location: homepage.php");
+//}
+//else {
+//    echo("ERRORERROEREROERO");
+//}
+    
 ?>
+
--- nav.php
+++ nav.php
@@ -18,6 +18,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
+                <?php print_r($_SESSION); ?>
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
