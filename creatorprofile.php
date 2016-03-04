1e12e9d543a939814183590bfd5ef67cbb8fbb4e
lenadis <lenadisr@gmail.com>
2016-03-04 09:21:16 +0100

    creator page

--- creatorprofile.php
+++ creatorprofile.php
@@ -13,27 +13,27 @@
                 
                  <div>
   <img src="img/<?php echo $_SESSION['user_type'] . "/" . $_SESSION['user_id'] . ".jpg"; ?>" alt="picture"/>
+                     <h2><?php echo $_SESSION['name'] ?></h2>
 </div>   
                 <h2>HERE ARE YOUR COURSES</h2>
-                <h2><?php 
+                <?php 
                     $temp = $_SESSION['user_id'];
                     $query = $db->prepare("SELECT * FROM course WHERE creator_id= '".$temp. "'");
                     $query->execute();
                     $result = $query;
                         foreach ($query as $row) {
                             //var_dump($row);
+                            ?><h3><?php
                             echo $row["name"];
-                            echo "<br>";
+                            ?></h3><h5><?php
                             echo $row["description"];
-                            echo "<br>";
+                            ?></h5><?php
                         }
 }
-                ?></h2>
-                    
+                ?>
+                <br><a href='createcourse.php'>Add a new one</a>   
                 <div class="col-xs-6 nopadding maxheightandwidth">
                 
-                
-                <p><a href='createcourse.php'>Add a new one</a></p>
                    
                 <p class="big-center"> TEACH </p>
                 <a class="subtitle btn btn-default" href="signupcreators.php"> Sign up </a> <br>
